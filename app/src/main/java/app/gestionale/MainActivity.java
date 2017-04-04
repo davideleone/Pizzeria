7d8fd0f35f1b799bb43c1d7d68648990c398e95e
Davide Leone <davideleone94@gmail.com>
2017-04-04 17:12:35 +0200

    Navigation Drawer Setup, Fragments added

---
+++ app/src/main/java/app/gestionale/MainActivity.java
@@ -0,0 +1,171 @@
+package app.gestionale;
+
+import android.app.AlertDialog;
+import android.app.Fragment;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawer;
+    private Toolbar toolbar;
+    private NavigationView nvDrawer;
+    private ActionBarDrawerToggle drawerToggle;
+    private ImageView logo;
+    private FrameLayout frameLayout;
+    private RelativeLayout layoutAperti;
+    private RelativeLayout layoutChiusi;
+    private Calendar c = Calendar.getInstance();
+    private int seconds = c.get(Calendar.SECOND);
+    private int hour = c.get(Calendar.HOUR_OF_DAY);
+    private int day = c.get(Calendar.DAY_OF_WEEK);
+    private TextView nomeAct;
+    ArrayList<String> listaPizze;
+    ArrayList<String> listaIngredienti;
+    ArrayList<String> listaPrezzi;
+    private String idOrdine = "";
+    private String modifica = "";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        frameLayout = (FrameLayout) findViewById(R.id.flContent);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        nvDrawer = (NavigationView) findViewById(R.id.nvView);
+        View hView = nvDrawer.getHeaderView(0);
+
+        setupDrawerContent(nvDrawer);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerToggle = setupDrawerToggle();
+        mDrawer.addDrawerListener(drawerToggle);
+
+        //logo = (ImageView) findViewById(R.id.logo_accipizza);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+    }
+
+
+    public void selectDrawerItem(MenuItem menuItem) {
+
+        Fragment fragment = null;
+        Class fragmentClass;
+        switch (menuItem.getItemId()) {
+            case R.id.nav_first_fragment:
+                fragmentClass = RiepilogoOrdini.class;
+                break;
+            /*case R.id.nav_second_fragment:
+                //fragmentClass = MonitoraOrdine.class;
+                break;
+            case R.id.nav_third_fragment:
+                //fragmentClass = Contatti.class;
+                break;
+            case R.id.nav_logout:
+                break;*/
+            default:
+                fragmentClass = MainActivity.class;
+        }
+
+        try {
+            if (fragmentClass != null)
+                fragment = (Fragment) fragmentClass.newInstance();
+            else fragment = null;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Insert the fragment by replacing any existing fragment
+        if (fragment != null) {
+            getFragmentManager().beginTransaction().replace(R.id.flContent, fragment).commit();
+            // Highlight the selected item has been done by NavigationView
+            menuItem.setChecked(false);
+            setTitle(menuItem.getTitle());
+            mDrawer.closeDrawers();
+        }
+
+    }
+
+    private ActionBarDrawerToggle setupDrawerToggle() {
+        return new ActionBarDrawerToggle(this, mDrawer, toolbar, R.string.drawer_open, R.string.drawer_close);
+    }
+
+    @Override
+
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggles
+        drawerToggle.onConfigurationChanged(newConfig);
+        layoutTheView();
+    }
+
+    private void layoutTheView() {
+        ActionBar actionBar = this.getSupportActionBar();
+        ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) frameLayout.getLayoutParams();
+        int actionBarHeight = actionBar.getHeight();
+        params.setMargins(0, actionBarHeight, 0, 0);
+        frameLayout.setLayoutParams(params);
+        frameLayout.requestLayout();
+    }
+
+
+    @Override
+    public void onBackPressed() {
+    }
+
+}
--- app/src/main/java/app/gestionale/RiepilogoOrdini.java
+++ app/src/main/java/app/gestionale/RiepilogoOrdini.java
@@ -1,8 +1,18 @@
 package app.gestionale;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Fragment;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
 import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
@@ -19,20 +29,45 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 
-public class RiepilogoOrdini extends AppCompatActivity {
+public class RiepilogoOrdini extends Fragment {
 
     private Spinner listadate;
     private ArrayAdapter<String> listaDate;
     private TableLayout tabellaOrdini;
+    private FragmentActivity listener;
+    private Bundle bundle;
+    private Context context;
+    private String fattorini[]= {"Matteo", "Mirko", "Davide"};
+    private Spinner listafattorini;
+    private ArrayAdapter<String> listaFattorini;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_riepilogo_ordini);
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof Activity) {
+            this.listener = (FragmentActivity) context;
+        }
+    }
 
-        tabellaOrdini = (TableLayout) findViewById(R.id.tabella_ordini);
-        listadate = (Spinner) findViewById(R.id.date_settimane);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+        bundle = this.getArguments();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View view = inflater.inflate(R.layout.activity_riepilogo_ordini, container, false);
+        context = view.getContext();
+        super.onCreate(savedInstanceState);
+
+        tabellaOrdini = (TableLayout) view.findViewById(R.id.tabella_ordini);
+        listadate = (Spinner) view.findViewById(R.id.date_settimane);
 
         String[] dateSettimana;
         Calendar c = Calendar.getInstance(Locale.ITALY);
@@ -57,7 +92,7 @@ public class RiepilogoOrdini extends AppCompatActivity {
 
         dateSettimana[dateSettimana.length - 1] = giorno + "-" + mese + "-" + anno;
 
-        listaDate = new ArrayAdapter<String>(this, R.layout.content_spinner, dateSettimana); //selected item will look like a spinner set from XML
+        listaDate = new ArrayAdapter<String>(context, R.layout.date_spinner, dateSettimana); //selected item will look like a spinner set from XML
         listaDate.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         listadate.setAdapter(listaDate);
 
@@ -70,52 +105,132 @@ public class RiepilogoOrdini extends AppCompatActivity {
             }
         });*/
 
-        caricaOrdini();
+        listafattorini = new Spinner(context);
+        listaFattorini = new ArrayAdapter<String>(context, R.layout.fattorini_spinner, fattorini); //selected item will look like a spinner set from XML
+        listaFattorini.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        listafattorini.setAdapter(listaFattorini);
+
+        caricaOrdini(listadate.getSelectedItem().toString());
+        return view;
     }
 
-    public void caricaOrdini() {
+    public void caricaOrdini(String data) {
         List<HashMap<String, Object>> risultatoQuery2;
-        risultatoQuery2 = DBmanager.selectQuery(EnumQuery.MONITORA_ORDINE.getValore(), listadate.getSelectedItem().toString());
+        risultatoQuery2 = DBmanager.selectQuery(EnumQuery.MONITORA_ORDINE.getValore(), data);
         Iterator<HashMap<String, Object>> itr2 = risultatoQuery2.iterator();
-    if(itr2.hasNext()) {
-        while (itr2.hasNext()) {
-            HashMap<String, Object> riga = itr2.next();
-            final String idOrdine = riga.get("id").toString();
-            final String stato = riga.get("tipo").toString();
-            final String consegna = riga.get("consegna").toString();
-            final String totale = new DecimalFormat("#0.00").format((double) Float.parseFloat(riga.get("totale").toString())) + " \u20ac";
-            final String social = riga.get("social").toString();
+        if (itr2.hasNext()) {
+            while (itr2.hasNext()) {
+                HashMap<String, Object> riga = itr2.next();
+                final String cognome = riga.get("cognome").toString();
+                final String idOrdine = riga.get("id").toString();
+                final String stato = riga.get("tipo").toString();
+                final String consegna = riga.get("consegna").toString();
+                final String totale = new DecimalFormat("#0.00").format((double) Float.parseFloat(riga.get("totale").toString())) + " \u20ac";
+                final String social = riga.get("social").toString();
 
-            TableRow row = new TableRow(this);
-            row.setBackgroundResource(R.drawable.table_bottom_style);
+                TableRow row = new TableRow(context);
+                row.setBackgroundResource(R.drawable.table_bottom_style);
 
-            TextView dataOrdine = makeTableRowWithText(riga.get("dataconsegna").toString(), R.dimen.dim_150dp);
-            TextView oraOrdine = makeTableRowWithText(riga.get("oraconsegna").toString().substring(0, 5), R.dimen.dim_80dp);
-            TextView nomeCliente = makeTableRowWithText((riga.get("nome").toString()), R.dimen.dim_150dp);
-            TextView cognomeCliente = makeTableRowWithText(riga.get("cognome").toString(), R.dimen.dim_150dp);
-            TextView totaleOrdine = makeTableRowWithText(totale, R.dimen.dim_100dp);
-            TextView viaOrdine = makeTableRowWithText(riga.get("via").toString(), R.dimen.dim_150dp);
-            TextView cittaOrdine = makeTableRowWithText(riga.get("citta").toString(), R.dimen.dim_150dp);
+                TextView dataOrdine = makeTableRowWithText(riga.get("dataconsegna").toString(), R.dimen.dim_150dp);
+                TextView oraOrdine = makeTableRowWithText(riga.get("oraconsegna").toString().substring(0, 5), R.dimen.dim_80dp);
+                TextView nomeCliente = makeTableRowWithText((riga.get("nome").toString()), R.dimen.dim_150dp);
+                TextView cognomeCliente = makeTableRowWithText(cognome, R.dimen.dim_150dp);
+                TextView totaleOrdine = makeTableRowWithText(totale, R.dimen.dim_100dp);
+                TextView viaOrdine = makeTableRowWithText(riga.get("via").toString(), R.dimen.dim_200dp);
+                TextView cittaOrdine = makeTableRowWithText(riga.get("citta").toString(), R.dimen.dim_150dp);
 
-            row.addView(dataOrdine);
-            row.addView(oraOrdine);
-            row.addView(nomeCliente);
-            row.addView(cognomeCliente);
-            row.addView(totaleOrdine);
-            row.addView(viaOrdine);
-            row.addView(cittaOrdine);
+                ImageButton btnMostra = makeTableRowWithImageButton(R.drawable.mostra);
+                ImageButton btnAccetta = makeTableRowWithImageButton(R.drawable.accetta);
+                ImageButton btnConsegna = makeTableRowWithImageButton(R.drawable.consegna);
+                ImageButton btnElimina = makeTableRowWithImageButton(R.drawable.elimina);
 
-            tabellaOrdini.addView(row);
-        }
-    }
-    else Toast.makeText(this, "Nessun nuovo ordine", Toast.LENGTH_SHORT).show();
+
+                btnConsegna.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        RelativeLayout layoutDialog = new RelativeLayout(context);
+                        RelativeLayout.LayoutParams spinnerParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                        spinnerParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+                        spinnerParams.addRule(RelativeLayout.CENTER_VERTICAL);
+                        listafattorini.setLayoutParams(spinnerParams);
+                        layoutDialog.addView(listafattorini);
+
+                        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                        builder.setTitle("Scelta Fattorino");
+                        builder.setView(layoutDialog);
+                        builder.setPositiveButton("Consegna", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        });
+                        builder.setNegativeButton("Annulla", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.cancel();
+                                    }
+                                });
+                        builder.create().show();
+                    }
+                });
+
+                btnAccetta.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        DBmanager.updateQuery(EnumQuery.ACCETTA_ORDINE.getValore(), false, idOrdine);
+                    }
+                });
+
+
+                btnElimina.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        new AlertDialog.Builder(context)
+                                .setTitle("Elimina ordine")
+                                .setMessage("Sei sicuro di voler eliminare l'ordine di "+cognome+"?")
+                                .setPositiveButton("Si, elimina", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        DBmanager.updateQuery(EnumQuery.ELIMINA_ORDINE0.getValore(), false, idOrdine);
+                                        DBmanager.updateQuery(EnumQuery.ELIMINA_ORDINE1.getValore(), false, idOrdine);
+                                        DBmanager.updateQuery(EnumQuery.ELIMINA_ORDINE2.getValore(), false, idOrdine);
+                                        DBmanager.updateQuery(EnumQuery.ELIMINA_ORDINE3.getValore(), false, idOrdine);
+                                        tabellaOrdini.removeAllViews();
+                                        caricaOrdini(listadate.getSelectedItem().toString());
+                                        Toast.makeText(context, "Ordine Eliminato!", Toast.LENGTH_SHORT).show();
+                                    }
+                                })
+                                .setNegativeButton("Annulla", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.cancel();
+                                    }
+                                })
+                                .setIcon(R.drawable.logo)
+                                .show();
+
+                    }
+                });
+
+                row.addView(dataOrdine);
+                row.addView(oraOrdine);
+                row.addView(nomeCliente);
+                row.addView(cognomeCliente);
+                row.addView(totaleOrdine);
+                row.addView(viaOrdine);
+                row.addView(cittaOrdine);
+                row.addView(btnMostra);
+                row.addView(btnAccetta);
+                row.addView(btnConsegna);
+                row.addView(btnElimina);
+
+                tabellaOrdini.addView(row);
+            }
+        } else Toast.makeText(context, "Nessun nuovo ordine", Toast.LENGTH_SHORT).show();
     }
 
     private TextView recyclableTextView;
     private ImageButton recyclableImageButton;
 
     private TextView makeTableRowWithText(String text, int resource) {
-        recyclableTextView = new TextView(this);
+        recyclableTextView = new TextView(context);
         recyclableTextView.setGravity(Gravity.CENTER);
         recyclableTextView.setText(text);
         recyclableTextView.setTextSize(25);
@@ -124,12 +239,12 @@ public class RiepilogoOrdini extends AppCompatActivity {
     }
 
     private ImageButton makeTableRowWithImageButton(int img) {
-        TableLayout.LayoutParams ImageButtonLayout = new TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.WRAP_CONTENT);
-        ImageButtonLayout.setMargins(10, 0, 0, 0);
-        recyclableImageButton = new ImageButton(this);
-        recyclableImageButton.setLayoutParams(ImageButtonLayout);
-        recyclableImageButton.setBackgroundResource(R.drawable.button);
+        recyclableImageButton = new ImageButton(context);
+        recyclableImageButton.setMinimumWidth(getResources().getDimensionPixelSize(R.dimen.dim_45dp));
+        recyclableImageButton.setImageResource(img);
+        recyclableImageButton.setBackgroundColor(Color.TRANSPARENT);
         return recyclableImageButton;
     }
+
 
 }
