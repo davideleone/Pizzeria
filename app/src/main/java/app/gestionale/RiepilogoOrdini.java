e149cbea6aff9cc1a1a6c98583d7a2eb7789e498
Davide Leone <davideleone94@gmail.com>
2017-04-22 12:23:31 +0200

    Fixed printing global class

--- app/src/main/java/app/gestionale/RiepilogoOrdini.java
+++ app/src/main/java/app/gestionale/RiepilogoOrdini.java
@@ -9,10 +9,8 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.FragmentActivity;
@@ -46,14 +44,11 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import java.util.Set;
-import java.util.UUID;
 
 import terranovaproductions.newcomicreader.FloatingActionMenu;
 
 public class RiepilogoOrdini extends Fragment {
 
-    Stampa stampa = new Stampa();
     TextView myLabel;
     // will enable user to enter any text to be printed
     EditText myTextbox;
@@ -356,10 +351,11 @@ public class RiepilogoOrdini extends Fragment {
                     .setPositiveButton("Stampa", new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int which) {
                             try {
-                                findBT();
-                                openBT();
-                                sendData("Riepilogo Serata del " + dataRicerca);
-                                //closeBT();
+                                Stampa stampa = new Stampa(getActivity(), context);
+                                stampa.findBT();
+                                stampa.openBT();
+                                stampa.sendData("Riepilogo Serata del " + dataRicerca);
+                                stampa.closeBT();
 
                             } catch (IOException ex) {
                                 ex.printStackTrace();
@@ -825,165 +821,6 @@ public class RiepilogoOrdini extends Fragment {
         recyclableImageButton.setImageResource(img);
         recyclableImageButton.setBackgroundColor(Color.TRANSPARENT);
         return recyclableImageButton;
-    }
-
-
-    // this will find a bluetooth printer device
-    void findBT() {
-
-        try {
-            mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-
-            if (mBluetoothAdapter == null) {
-                Toast.makeText(context, "BlueTooth Printer non trovato!", Toast.LENGTH_SHORT).show();
-            }
-
-            if (!mBluetoothAdapter.isEnabled()) {
-                Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                startActivityForResult(enableBluetooth, 0);
-            }
-
-            Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-
-            if (pairedDevices.size() > 0) {
-                for (BluetoothDevice device : pairedDevices) {
-
-                    // RPP300 is the name of the bluetooth printer device
-                    // we got this name from the list of paired devices
-                    if (device.getName().equals("BlueTooth Printer")) {
-                        mmDevice = device;
-                        break;
-                    }
-                }
-            }
-
-            Toast.makeText(context, "BlueTooth Printer trovato!", Toast.LENGTH_SHORT).show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    // tries to open a connection to the bluetooth printer device
-    void openBT() throws IOException {
-        try {
-
-            // Standard SerialPortService ID
-            UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
-            mmSocket = mmDevice.createRfcommSocketToServiceRecord(uuid);
-            mmSocket.connect();
-            mmOutputStream = mmSocket.getOutputStream();
-            mmInputStream = mmSocket.getInputStream();
-
-            beginListenForData();
-
-            Toast.makeText(context, "BlueTooth Printer associato!", Toast.LENGTH_SHORT).show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    /*
- * after opening a connection to bluetooth printer device,
- * we have to listen and check if a data were sent to be printed.
- */
-    void beginListenForData() {
-        try {
-            final Handler handler = new Handler();
-
-            // this is the ASCII code for a newline character
-            final byte delimiter = 10;
-
-            stopWorker = false;
-            readBufferPosition = 0;
-            readBuffer = new byte[1024];
-
-            workerThread = new Thread(new Runnable() {
-                public void run() {
-
-                    while (!Thread.currentThread().isInterrupted() && !stopWorker) {
-
-                        try {
-
-                            int bytesAvailable = mmInputStream.available();
-
-                            if (bytesAvailable > 0) {
-
-                                byte[] packetBytes = new byte[bytesAvailable];
-                                mmInputStream.read(packetBytes);
-
-                                for (int i = 0; i < bytesAvailable; i++) {
-
-                                    byte b = packetBytes[i];
-                                    if (b == delimiter) {
-
-                                        byte[] encodedBytes = new byte[readBufferPosition];
-                                        System.arraycopy(
-                                                readBuffer, 0,
-                                                encodedBytes, 0,
-                                                encodedBytes.length
-                                        );
-
-                                        // specify US-ASCII encoding
-                                        final String data = new String(encodedBytes, "US-ASCII");
-                                        readBufferPosition = 0;
-
-                                        // tell the user data were sent to bluetooth printer device
-                                        handler.post(new Runnable() {
-                                            public void run() {
-                                                myLabel.setText(data);
-                                            }
-                                        });
-
-                                    } else {
-                                        readBuffer[readBufferPosition++] = b;
-                                    }
-                                }
-                            }
-
-                        } catch (IOException ex) {
-                            stopWorker = true;
-                        }
-
-                    }
-                }
-            });
-
-            workerThread.start();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    // this will send text data to be printed by the bluetooth printer
-    void sendData(String testoDaStampare) throws IOException {
-        try {
-
-            // the text typed by the user
-            String msg = testoDaStampare;
-            msg += "\n";
-
-            mmOutputStream.write(msg.getBytes());
-
-            // tell the user data were sent
-            Toast.makeText(context, "Testo inviato!", Toast.LENGTH_SHORT).show();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    // close the connection to bluetooth printer.
-    void closeBT() throws IOException {
-        try {
-            stopWorker = true;
-            mmOutputStream.close();
-            mmInputStream.close();
-            mmSocket.close();
-            Toast.makeText(context, "BlueTooth Printer disassociato!", Toast.LENGTH_SHORT).show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
 }
